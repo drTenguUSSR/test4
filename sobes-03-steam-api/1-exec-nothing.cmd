5b14b645b337c7835f4ed4bebf41b90bcb8c0ab9
drTengu <162064018+drTenguUSSR@users.noreply.github.com>
2024-06-26 01:14:07 +0300

    sobes03. build test and run fixup

--- sobes-03-steam-api/1-exec-nothing.cmd
+++ sobes-03-steam-api/1-exec-nothing.cmd
@@ -1,2 +1,3 @@
 @rem call 0-make-jar.cmd
-java -cp target/sobes-03-steam-api-1.0-SNAPSHOT.jar mil.teng.sobes03.bench.BenchNothing
+@rem java -cp target/sobes-03-steam-api-1.0-SNAPSHOT.jar mil.teng.sobes03.bench.BenchNothing
+java -jar target/benchmarks.jar
